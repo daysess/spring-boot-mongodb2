649bf15269081dac21d0acfe1c103b9e26fdb3b6
Dayse Soares <daysedesign@gmail.com>
2020-09-08 21:52:16 -0300

    Primeiro commit - projeto criado.

---
+++ src/test/java/br/com/daysesoares/workshopmongodb/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
