3833a38817aed22cd41afb0a011ed657d87f6f6c
Dayse Soares <daysedesign@gmail.com>
2020-09-14 21:50:44 -0300

    Obtendo um usuario por id

--- src/main/java/br/com/daysesoares/workshopmongodb/services/UserService.java
+++ src/main/java/br/com/daysesoares/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.daysesoares.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.daysesoares.workshopmongodb.domain.User;
 import br.com.daysesoares.workshopmongodb.repositoy.UserRepository;
+import br.com.daysesoares.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,14 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		if(!user.isPresent()) {
+			throw new ObjectNotFoundException("Usuário não encontrado.");
+		}
+		return user.get();
 	}
 
 }
