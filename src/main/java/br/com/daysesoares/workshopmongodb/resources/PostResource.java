d19847c2949f6cbef15f25ce5db5a15d2396af80
Dayse Soares <daysedesign@gmail.com>
2020-09-17 20:22:27 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/daysesoares/workshopmongodb/resources/PostResource.java
@@ -0,0 +1,27 @@
+package br.com.daysesoares.workshopmongodb.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.daysesoares.workshopmongodb.domain.Post;
+import br.com.daysesoares.workshopmongodb.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService postService;
+		
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post obj = postService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+		
+
+}
---
+++ src/main/java/br/com/daysesoares/workshopmongodb/services/PostService.java
@@ -0,0 +1,26 @@
+package br.com.daysesoares.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.daysesoares.workshopmongodb.domain.Post;
+import br.com.daysesoares.workshopmongodb.repositoy.PostRepository;
+import br.com.daysesoares.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		if(!post.isPresent()) {
+			throw new ObjectNotFoundException("Post não encontrado.");
+		}
+		return post.get();
+	}
+	
+}
