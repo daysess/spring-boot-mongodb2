48f5b5e9f87d02633ec9909bc70f25eb15bf2b4d
Dayse Soares <daysedesign@gmail.com>
2020-09-24 19:39:00 -0300

    Consulta simples com @Query

--- src/main/java/br/com/daysesoares/workshopmongodb/repositoy/PostRepository.java
+++ src/main/java/br/com/daysesoares/workshopmongodb/repositoy/PostRepository.java
@@ -3,6 +3,7 @@ package br.com.daysesoares.workshopmongodb.repositoy;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.daysesoares.workshopmongodb.domain.Post;
@@ -10,6 +11,9 @@ import br.com.daysesoares.workshopmongodb.domain.Post;
 @Repository
 public interface PostRepository  extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContaining(String text);
 	
 }
