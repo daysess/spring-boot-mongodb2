3f9f186b21ed02b0d748eae427e34b8f0c1e1b3a
Dayse Soares <daysedesign@gmail.com>
2020-09-23 22:38:11 -0300

    Consulta simples com query methods

--- src/main/java/br/com/daysesoares/workshopmongodb/repositoy/PostRepository.java
+++ src/main/java/br/com/daysesoares/workshopmongodb/repositoy/PostRepository.java
@@ -1,5 +1,7 @@
 package br.com.daysesoares.workshopmongodb.repositoy;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import br.com.daysesoares.workshopmongodb.domain.Post;
 @Repository
 public interface PostRepository  extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContaining(String text);
+	
 }
--- src/main/java/br/com/daysesoares/workshopmongodb/services/PostService.java
+++ src/main/java/br/com/daysesoares/workshopmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package br.com.daysesoares.workshopmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,4 +24,7 @@ public class PostService {
 		return post.get();
 	}
 	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContaining(text);
+	}
 }
